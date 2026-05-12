fb0ddb2eeb70d5c6ea2e436bcf5707ca6d16b7b0
Raseel6268 <bmrasel626825@gmail.com>
2026-05-12 15:49:51 +0600

    auth: sameSite

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,9 +47,9 @@ const login = async (req, res) => {
 
     res.cookie("token", result.token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
     });
     return res.status(200).json({
       message: "Login successful",
